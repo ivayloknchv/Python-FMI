2555764d5f22dd3cbbbebf3b0b279f822bca99d5
ivayloknchv <46057611+ivayloknchv@users.noreply.github.com>
2024-10-16 00:52:37 +0300

    Create HW1.py

---
+++ Homeworks/HW1.py
@@ -0,0 +1,20 @@
+viktors_ingredients = [
+    'чушки', 'домати', 'моркови', 'ябълки',
+    'сол', 'черен пипер', 'кимион', 'зехтин'
+]
+
+georgis_ingredients = (
+    'чушки', 'домати', 'патладжан', 'люти чушки', 'олио',
+    'захар', 'чубрица', 'черен пипер', 'врачанска ракия'
+)
+
+shopping_list = []
+shopping_list.extend(viktors_ingredients)
+shopping_list.extend(georgis_ingredients)
+shopping_list = shopping_list[::-1]
+
+unique_ingredients = set(shopping_list)
+ingredient_quantities = dict.fromkeys(unique_ingredients, 5)
+ingredient_quantities['skyr'] = 1
+
+number_of_ingredients_to_buy = len(ingredient_quantities)
