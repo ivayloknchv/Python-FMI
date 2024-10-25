406da7087d6e9cdbc78b271f2e379dff32be8e12
ivayloknchv <46057611+ivayloknchv@users.noreply.github.com>
2024-10-25 13:51:47 +0300

    Update vector.py

--- Code Snippets and Demos/vector.py
+++ Code Snippets and Demos/vector.py
@@ -5,15 +5,12 @@ class Vector:
         self.y = y
         self.z = z
 
-
     def _coordinates(self):
         return self.x, self.y, self.z
-
 
     @property
     def length(self):
         return sum(_ ** 2 for _ in self._coordinates()) ** 0.5
-
 
     @staticmethod
     def create_from_list(coordinates):
@@ -21,13 +18,11 @@ class Vector:
             return Vector(0, 0, 0)
         return Vector(coordinates[0], coordinates[1], coordinates[2])
 
-
     def normalize(self):
         vector_length = self.length
         self.x /=  vector_length
         self.y /=  vector_length
         self.z /=  vector_length
-
 
     def normalized(self):
         vector_length = self.length
@@ -36,17 +31,14 @@ class Vector:
         norm_z = self.z / vector_length
         return Vector(norm_x, norm_y, norm_z)
 
-
     def print_vector(self):
         print(f'({self.x}, {self.y}, {self.z})')
-
 
     def __iadd__(self, other):
         self.x += other.x
         self.y += other.y
         self.z += other.z
         return self
-
 
     def __add__(self, other):
         new_x = self.x + other.x
@@ -60,13 +52,11 @@ class Vector:
         self.z -= other.z
         return self
 
-
     def __sub__(self, other):
         new_x = self.x - other.x
         new_y = self.y - other.y
         new_z = self.z - other.z
         return  Vector(new_x, new_y, new_z)
-
 
     def __imul__(self, scalar):
         self.x *= scalar
@@ -74,13 +64,11 @@ class Vector:
         self.z *= scalar
         return self
 
-
     def __mul__(self, scalar):
         new_x = self.x * scalar
         new_y = self.y * scalar
         new_z = self.z * scalar
         return Vector(new_x, new_y, new_z)
-
 
     def __rmul__(self, scalar):
         return self.__mul__(scalar)
