d7839c9a7cb6278ebc7b63540ac1ae6269611f21
ivayloknchv <46057611+ivayloknchv@users.noreply.github.com>
2024-10-25 13:51:06 +0300

    Update countable.py

--- Code Snippets and Demos/countable.py
+++ Code Snippets and Demos/countable.py
@@ -1,21 +1,18 @@
 class Countable:
-
+    
     _count = 0
 
     def __init__(self, x):
         self.x = x
         type(self).increase_count()
 
-
     @classmethod
     def increase_count(cls):
         cls._count += 1
 
-
     @classmethod
     def decrease_count(cls):
         cls._count -= 1
-
 
     def __del__(self):
         type(self).decrease_count()
