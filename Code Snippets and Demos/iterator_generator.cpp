e3017293a19505aef388f37535ae144a5ef2d8c8
ivayloknchv <46057611+ivayloknchv@users.noreply.github.com>
2024-12-09 00:20:13 +0200

    Create iterator_generator.cpp

---
+++ Code Snippets and Demos/iterator_generator.cpp
@@ -0,0 +1,30 @@
+class Squarer:
+
+    def __init__(self, begin, end):
+        self.begin = begin
+        self.end = end
+
+    def __iter__(self):
+        return SquarerIterator(self.begin, self.end)
+
+#iterator
+class SquarerIterator:
+
+    def __init__(self, begin, end):
+        self.begin = begin
+        self.end = end
+
+    def __next__(self):
+        if self.begin <= self.end:
+            ans = self.begin ** 2
+            self.begin += 1
+            return ans
+        else:
+            raise StopIteration
+
+#generator
+def squarer_generator(begin, end):
+    current = begin
+    while current <= end:
+        yield  current ** 2
+        current += 1
